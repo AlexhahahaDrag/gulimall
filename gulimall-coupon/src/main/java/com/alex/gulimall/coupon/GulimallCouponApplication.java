2d33a24fff5b903fda992e8291bb8db05ce858a1
alexhahahaDrag <734663446@qq.com>
2020-09-27 22:16:11 +0800

    整合nacos注册中心和配置中心

--- gulimall-coupon/src/main/java/com/alex/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/alex/gulimall/coupon/GulimallCouponApplication.java
@@ -3,9 +3,11 @@ package com.alex.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.alex.gulimall.coupon.dao")
+@EnableDiscoveryClient
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
